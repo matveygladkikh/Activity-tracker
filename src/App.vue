82430dccfd4fc612588a369bc44fc6a3b009cf5d
Matvey Gladkikh <gladkih228236@gmail.com>
2025-08-30 00:52:50 +0700

    init tailwind + formatted

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <script setup></script>
 
 <template>
+  <h1 class="text-3xl font-bold text-red-500">Hello Tailwind</h1>
+
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
