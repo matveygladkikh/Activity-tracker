29a4e0d33cee3aaa868e1dd6d7044b44142b9ae3
Matvey Gladkikh <gladkih228236@gmail.com>
2025-09-15 00:46:26 +0700

    - feat + refactor(logic): added new time constants added dynamic generation of time for activity changed placeholder for activities

--- src/functions.js
+++ src/functions.js
@@ -1,5 +1,12 @@
 import { isPageValid, isNull } from './validators'
-import { PAGE_TIMELINE, HOURS_IN_DAY, MIDNIGHT_HOUR, SECONDS_IN_HOUR } from './constants'
+import {
+  PAGE_TIMELINE,
+  HOURS_IN_DAY,
+  MIDNIGHT_HOUR,
+  SECONDS_IN_HOUR,
+  SECONDS_IN_MINUTE,
+  MINUTES_IN_HOUR,
+} from './constants'
 
 export function normalizePageHash() {
   const page = window.location.hash.slice(1)
@@ -40,4 +47,21 @@ export function generateActivitySelectOptions(activities) {
 
 export function id() {
   return Date.now().toString(36) + Math.random().toString(36).substring(2)
+}
+
+export function generatePeriodSelectOptions(periodInMinutes) {
+  return periodInMinutes.map((periodInMinutes) => ({
+    value: periodInMinutes * SECONDS_IN_MINUTE,
+    label: generatePeriodSelectOptionsLabel(periodInMinutes),
+  }))
+}
+
+function generatePeriodSelectOptionsLabel(periodInMinutes) {
+  const hours = Math.floor(periodInMinutes / MINUTES_IN_HOUR)
+    .toString()
+    .padStart(2, 0)
+
+  const minutes = (periodInMinutes % MINUTES_IN_HOUR).toString().padStart(2, 0)
+
+  return `${hours}:${minutes}`
 }
