6829cc53ccb3c6c0e6753a5f49224449e86ace5c
Matvey Gladkikh <gladkih228236@gmail.com>
2025-10-01 23:42:16 +0700

    - feat(ui+logic): a new component, TheTimelineIndicator, has been added to TheTimeline navigation page. This timeline indicator is a line-shaped time indicator the indicator component contains logic for changing its position relative to the page, which depends on the percentage, which in turn depends on the time elapsed in a day (the indicator moves toward the end of the day) a new constant, SECONDS_IN_DAY, has been added to the constants.js module logic has been added via the watchEffect hook: when it reaches 100%, the indicator resets and starts counting from the beginning of the day logic has been added via the onActivated and onDeactivated hooks, which stops the indicator when switching to another page and restarts it with the current percentage of the elapsed day upon returning

---
+++ src/components/TheTimelineIndicator.vue
@@ -0,0 +1,59 @@
+<script setup>
+import { computed, ref, watchEffect, onActivated, onDeactivated } from 'vue'
+import {
+  HUNDRED_PERCENT,
+  MILLISECONDS_IN_SECOND,
+  MINUTES_IN_HOUR,
+  SECONDS_IN_DAY,
+  SECONDS_IN_MINUTE,
+} from '@/constants'
+
+const secondsSinceMidnight = ref(calculateSecondsSinceMidnight())
+const indicatorRef = ref()
+
+let timer = null
+
+onActivated(() => {
+  secondsSinceMidnight.value = calculateSecondsSinceMidnight()
+
+  timer = setInterval(() => (secondsSinceMidnight.value += 5 * 60), MILLISECONDS_IN_SECOND)
+})
+
+onDeactivated(() => clearInterval(timer))
+
+const topOffset = computed(
+  () => (secondsSinceMidnightInPercentage.value * getTimelineHeight()) / HUNDRED_PERCENT,
+)
+
+const secondsSinceMidnightInPercentage = computed(
+  () => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY,
+)
+
+watchEffect(() => {
+  if (secondsSinceMidnight.value > SECONDS_IN_DAY) {
+    secondsSinceMidnight.value = 0
+  }
+})
+
+function calculateSecondsSinceMidnight() {
+  const now = new Date()
+
+  return (
+    SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() +
+    SECONDS_IN_MINUTE * now.getMinutes() +
+    now.getSeconds()
+  )
+}
+
+function getTimelineHeight() {
+  return indicatorRef.value?.parentNode.getBoundingClientRect().height
+}
+</script>
+
+<template>
+  <hr
+    ref="indicatorRef"
+    class="pointer-event-none absolute z-10 w-full border-b-2 border-red-600"
+    :style="{ top: `${topOffset}px` }"
+  />
+</template>
--- src/constants.js
+++ src/constants.js
@@ -36,11 +36,12 @@ export const BUTTON_TYPES = [
 
 export const SECONDS_IN_MINUTE = 60
 export const MINUTES_IN_HOUR = 60
-export const SECONDS_IN_HOUR = SECONDS_IN_MINUTE * MINUTES_IN_HOUR
 export const MILLISECONDS_IN_SECOND = 1000
 
 export const MIDNIGHT_HOUR = 0
 export const HOURS_IN_DAY = 24
+export const SECONDS_IN_HOUR = SECONDS_IN_MINUTE * MINUTES_IN_HOUR
+export const SECONDS_IN_DAY = HOURS_IN_DAY * SECONDS_IN_HOUR
 
 export const LOW_PERCENT = 33
 export const MEDIUM_PERCENT = 6
