4d5fd861ae31c73b7dbc403152842a256f6f619b
Matvey Gladkikh <gladkih228236@gmail.com>
2025-10-02 02:05:19 +0700

    - refactor(logic): the time.js module has been added for the indicator's time logic the functions that control time have been moved from TheTimelineIndicator component to the time.js module. (Only hooks that accept functions from the time.js module remain in TheTimelineIndicator component) the watchEffects hook logic in the time.js module has been simplified and removed

--- src/components/TheTimelineIndicator.vue
+++ src/components/TheTimelineIndicator.vue
@@ -1,53 +1,20 @@
 <script setup>
-import { computed, ref, watchEffect, onActivated, onDeactivated } from 'vue'
-import {
-  HUNDRED_PERCENT,
-  MILLISECONDS_IN_SECOND,
-  MINUTES_IN_HOUR,
-  SECONDS_IN_DAY,
-  SECONDS_IN_MINUTE,
-} from '@/constants'
+import { ref, computed, onActivated, onDeactivated } from 'vue'
+import { startTimer, stopTimer, secondsSinceMidnightInPercentage } from '@/time'
+import { HUNDRED_PERCENT } from '@/constants'
 
-const secondsSinceMidnight = ref(calculateSecondsSinceMidnight())
 const indicatorRef = ref()
-
-let timer = null
-
-onActivated(() => {
-  secondsSinceMidnight.value = calculateSecondsSinceMidnight()
-
-  timer = setInterval(() => (secondsSinceMidnight.value += 5 * 60), MILLISECONDS_IN_SECOND)
-})
-
-onDeactivated(() => clearInterval(timer))
 
 const topOffset = computed(
   () => (secondsSinceMidnightInPercentage.value * getTimelineHeight()) / HUNDRED_PERCENT,
 )
 
-const secondsSinceMidnightInPercentage = computed(
-  () => (HUNDRED_PERCENT * secondsSinceMidnight.value) / SECONDS_IN_DAY,
-)
-
-watchEffect(() => {
-  if (secondsSinceMidnight.value > SECONDS_IN_DAY) {
-    secondsSinceMidnight.value = 0
-  }
-})
-
-function calculateSecondsSinceMidnight() {
-  const now = new Date()
-
-  return (
-    SECONDS_IN_MINUTE * MINUTES_IN_HOUR * now.getHours() +
-    SECONDS_IN_MINUTE * now.getMinutes() +
-    now.getSeconds()
-  )
-}
-
 function getTimelineHeight() {
   return indicatorRef.value?.parentNode.getBoundingClientRect().height
 }
+
+onActivated(startTimer)
+onDeactivated(stopTimer)
 </script>
 
 <template>
